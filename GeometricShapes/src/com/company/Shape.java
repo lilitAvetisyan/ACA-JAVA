25c142109c3ee278f5111c307793bc6554ebab55
Lilit Avetisyan <lilitavetisyan97@gmail.com>
2016-08-23 10:36:48 +0400

    remove .idea folder

--- GeometricShapes/src/com/company/Shape.java
+++ GeometricShapes/src/com/company/Shape.java
@@ -11,7 +11,6 @@ public abstract class Shape implements Comparable<Shape>
     public abstract double area();
     public abstract double perimeter();
 
-
     public static void main(String[] args)
     {
 
